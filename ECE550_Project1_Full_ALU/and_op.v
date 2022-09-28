6c90343707b615bb3eb0e2047c8b1b0e33b8018f
Xiuyuan Chen <74497884+XylonCh@users.noreply.github.com>
2022-09-28 14:22:09 -0400

    Add files via upload

---
+++ ECE550_Project1_Full_ALU/and_op.v
@@ -0,0 +1,13 @@
+module and_op(a,b,result);
+   input [31:0] a, b;
+   output [31:0] result;
+	
+   genvar i;
+   generate
+      for (i = 0; i < 32; i = i+1)
+      begin : and_a_b
+         and a1(result[i], a[i], b[i]);
+      end
+   endgenerate
+	
+endmodule
